672cb38ffb96a315ad074371b37b4a0d9693cd43
Matys <m.waszkiewicz88@o2.pl>
2018-08-26 10:56:32 +0200

    Add docker compose

--- src/models/order.js
+++
@@ -1,8 +0,0 @@
-export default (sequelize, DataTypes) => {
-  var Order = sequelize.define('Order', {
-    type: DataTypes.STRING,
-    name: DataTypes.STRING,
-    quantity: DataTypes.INTEGER
-  });
-  return Order;
-};
