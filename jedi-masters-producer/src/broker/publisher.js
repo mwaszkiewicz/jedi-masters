96df36f8895be5e662ffd1344de45a2d775def6e
Matys <m.waszkiewicz88@o2.pl>
2018-10-18 21:09:18 +0200

    Producer cleanup

--- jedi-masters-producer/src/broker/publisher.js
+++ jedi-masters-producer/src/broker/publisher.js
@@ -1,21 +1,20 @@
 import RabbitConnector from './connector';
 
 export default class Publisher {
-
     constructor() {
         this._connector = new RabbitConnector();
         this._connector.connect();
     }
 
   publish = (msg, queue) => {
-      if (this._connector !== undefined) {
-          let r = this._connector.publish(msg, queue);
-          console.log(r);
-        //.then(() => {
-        //       console.log('ok');
-        //   }).catch((err) => {
-        //       console.log(err);
-        //   });
-      }
-  };
+      return new Promise((resolve, reject) => {
+          try {
+              this._connector.publish(msg, queue);
+              resolve();
+          }catch (error) {
+              console.log(error);
+              reject(error);
+          }
+      });
+  }
 }
--- jedi-masters-producer/src/routes/action.js
+++ jedi-masters-producer/src/routes/action.js
@@ -5,15 +5,15 @@ const publisher = new Publisher();
 const action = (req, res, schema, queue) => {
     schema.validate(req.body, { abortEarly: false })
         .then((validatedReq) => {
-            publisher.publish(validatedReq, queue);
-                // .then((msg) => {
-                //     console.log(msg);
-                //     res.status(201).send(msg);
-                //     console.log(res);
-                // })
-                // .catch((err) => {
-                //     res.status(400).send(err);
-                // });
+            publisher.publish(validatedReq, queue)
+                .then((msg) => {
+                    console.log(msg);
+                    res.status(201).send(msg);
+                    console.log(res);
+                })
+                .catch((err) => {
+                    res.status(400).send(err);
+                });
         })
         .catch((validationError) => {
             console.log(validationError);
